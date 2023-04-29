592ca192a1daec46430479896262e5f8aa8f138d
Nosa Agho <nosaoasis@gmail.com>
2023-04-29 10:55:27 +0100

    Experimenting Move Commands
    Using the setTimeout and setInterval functions, data was sent to the server at set times and regular intervals.

--- client.js
+++ client.js
@@ -4,21 +4,38 @@ const net = require("net");
 const connect = function () {
   const conn = net.createConnection({
     host: "localhost",
-    port: 50541
+    port: 50541,
   });
 
   // interpret incoming data as text
   conn.setEncoding("utf8");
 
-  conn.on("data", data => {
-    console.log("You received data: ", data)
-  })
+  conn.on("data", (data) => {
+    console.log("You received data: ", data);
+  });
 
   conn.on("connect", () => {
-    conn.write("Name: NOA")
-  })
+    conn.write("Name: NOA");
+
+    setTimeout(() => {
+      conn.write("Move: up");
+    }, 1000);
+    setTimeout(() => {
+      conn.write("Move: right");
+    }, 2000);
+    setTimeout(() => {
+      conn.write("Move: down");
+    }, 3000);
+    setTimeout(() => {
+      conn.write("Move: left");
+    }, 4000);
+
+    setInterval(() => {
+      conn.write("Move: up");
+    }, 5000);
+  });
 
   return conn;
 };
 
-module.exports = { connect}
+module.exports = { connect };
