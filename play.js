e908c54606451c8a43299bf1a2b1b8ba9d3e072d
Nosa Agho <nosaoasis@gmail.com>
2023-04-29 11:13:44 +0100

    Refactoring
    Created an input.js file and moved the setupInput and handle user input functions into the input.js file. I added the setupInput function to the module.exports object and then imported/required it in the play.js file.

--- play.js
+++ play.js
@@ -1,22 +1,9 @@
 const {connect} = require("./client")
+const {setupInput} = require("./input")
 
-const handleUserInput = key => {
-  if (key === '\u0003') {
-    process.exit();
-  }
-}
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
 
-  stdin.on("data", (key) => handleUserInput(key))
-  return stdin;
-};
 
 console.log("Connecting ...");
 connect();
 setupInput()
 
-module.exports = {setupInput}
