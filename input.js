f415572723a0e27075a264ff935440d34c1e365a
Nosa Agho <nosaoasis@gmail.com>
2023-04-29 11:21:45 +0100

    Snake Movement
    Add the move commands to make the snake move in the direction specified by the user input.

--- input.js
+++ input.js
@@ -1,9 +1,19 @@
+let connection;
+
 const handleUserInput = (key) => {
-  if (key === "\u0003") {
-    process.exit();
-  }
+  if (key === "\u0003") process.exit();
+
+  if (key === "w") connection.write("Move: up");
+
+  if (key === "a") connection.write("Move: left");
+
+  if (key === "s") connection.write("Move: down");
+
+  if (key === "d") connection.write("Move: right");
+
+  if (key === "r") connection.write("Say: Lets Go!");
 };
-const setupInput = function () {
+const setupInput = function (conn) {
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
--- play.js
+++ play.js
@@ -4,6 +4,6 @@ const {setupInput} = require("./input")
 
 
 console.log("Connecting ...");
-connect();
-setupInput()
+const connection = connect();
+setupInput(connection)
 
